14d2611ffef5872ac5bed9b072f87bff2cc0c028
Ethandrake Castillo <ie714410@iteso.mx>
2020-11-13 05:39:25 -0700

    codec address added

---
+++ WM8731.c
@@ -0,0 +1,8 @@
+/*
+ * WM8731.c
+ *
+ *  Created on: Nov 13, 2020
+ *      Author: eduar
+ */
+
+#include "WM8731.h"
---
+++ WM8731.h
@@ -0,0 +1,23 @@
+/*
+ * WM8731.h
+ *
+ *  Created on: Nov 13, 2020
+ *      Author: eduar
+ */
+
+#ifndef WM8731_H_
+#define WM8731_H_
+
+#include "freertos_i2c.h"
+#include "fsl_clock.h"
+#include "fsl_port.h"
+#include "stdint.h"
+#include "FreeRTOS.h"
+#include "semphr.h"
+#include "task.h"
+
+#define WM8731_ADDRESS 0x1A
+
+
+
+#endif /* WM8731_H_ */
